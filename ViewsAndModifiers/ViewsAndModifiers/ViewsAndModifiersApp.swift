3f46d1eccbb85411c2771bbfa54d73fcfbe83598
Tausifqureshi786 <qamin9333@gmail.com>
2023-01-17 15:19:22 -0700

    Examples of utilizing custom modifiers, View Composition and extending Views to use custom modifiers easily.

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Tausif Qureshi on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
